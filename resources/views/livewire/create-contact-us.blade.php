cf3113f03b256320090fa36bda3f2da8c552328b
Mitesh Rathod <miteshr32@gmail.com>
2024-05-26 00:02:46 +0530

    added back button

--- resources/views/livewire/create-contact-us.blade.php
+++ resources/views/livewire/create-contact-us.blade.php
@@ -8,8 +8,10 @@
         {{-- <button type="submit">
             Submit
         </button> --}}
-        <div class="mt-3">
+        <div class="mt-3 flex gap-3">
             {{ $this->saveFormActions }}
+
+            <a href="{{ route('welcome') }}" class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">Back</a>
         </div>
     </form>
 
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('contact-us', ContactUs::class)->name('contact-us');
 
