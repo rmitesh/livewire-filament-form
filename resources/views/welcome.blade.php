09c5fced0c3553c42b36095550da84ff53cbcdaf
Mitesh Rathod <miteshr32@gmail.com>
2024-05-26 00:06:04 +0530

    added favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="apple-touch-icon" sizes="180x180" href="https://laravel.com/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://laravel.com/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://laravel.com/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="https://laravel.com/img/favicon/site.webmanifest">
+        <link rel="mask-icon" href="https://laravel.com/img/favicon/safari-pinned-tab.svg" color="#ff2d20">
+        <link rel="shortcut icon" href="https://laravel.com/img/favicon/favicon.ico">
+
         <title>Laravel</title>
 
         <!-- Fonts -->
